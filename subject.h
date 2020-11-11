ed0f86999869acbe3c4c9eb438f214ccd0a132c1
HURJIWAN <37547344+HURJIWAN@users.noreply.github.com>
2020-11-11 22:09:56 +0900

    Update subject.h

--- subject.h
+++ subject.h
@@ -32,6 +32,7 @@ protected:
 	float TemEff(int _level, float _Study, float _Sleep); // 레벨,  공부시간, 수면시간을 입력받아서 효율을 계산해서 리턴해줌. Subject의 효율은 변하지 않음. 
 
 	void SetSub(std::string); // 과목명 변경 함수
+	std::string GetSubName() { return subject; }
 
 	float GetStudyTime() { return TotalStudy; }; // 공부시간 접근 함수
 	void SetStudyTime(float); // 공부시간 설정 함수, 입력값 만큼 TotalStudy가 늘어난다.
