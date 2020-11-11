4c7b95c9522ab32b2dd662b017f22d034350b3c1
Seungwoo0707 <70153424+Seungwoo0707@users.noreply.github.com>
2020-11-11 21:57:15 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -30,13 +30,13 @@ void Test::TodayResult(Subject& _sub, Person& _person) {
 
 		std::cout << "D-" << dayNum << ". 내일이 기말고사입니다!" << std::endl; //날짜 출력
 		std::cout << "당신의 현재 Level은 " << _person.level << "입니다." << std::endl; //레벨 출력
-		std::cout << 과목명 << " 과목에서 당신의 성취도는 " << _sub.GetAchievement() << "입니다." << std::endl; //성취도 출력
-		std::cout << 과목명 << " 시험의 예상되는 학점은 " << ExpGrad << "입니다." << std::endl; // 예상 학점 출력
+		std::cout << _sub.GetSubName() << " 과목에서 당신의 성취도는 " << _sub.GetAchievement() << "입니다." << std::endl; //성취도 출력
+		std::cout << _sub.GetSubName() << " 시험의 예상되는 학점은 " << ExpGrad << "입니다." << std::endl; // 예상 학점 출력
 	}
 	else {
 		std::cout << "D-" << dayNum << std::endl; //날짜 출력
 		std::cout << "당신의 현재 Level은 " << _person.level << "입니다." << std::endl; //레벨 출력
-		std::cout << 과목명 << " 과목에서 당신의 성취도는 " << GetAchievement() << "입니다." << std::endl; //성취도 출력
+		std::cout << _sub.GetSubName() << " 과목에서 당신의 성취도는 " << GetAchievement() << "입니다." << std::endl; //성취도 출력
 	}
 	std::cout << "--------------------------------------------------------" << std::endl;
 }
@@ -45,13 +45,13 @@ void Test::TodayResult(Subject& _sub, Person& _person) {
 void Test::AfterTest(Subject& _sub, int score) {
 	float ExpScore = 10 * GetAchievement(); // 성취도 기반 예상 점수
 	if ( score < ExpScore && ( ExpScore - score ) / score > 0.3 ) { //성취도 > 시험 점수 && 오차가 일정 이상일 때 =>본인의 과대평가
-		std::cout << 과목명 << " 과목에 대해 당신은 당신 스스로를 과대평가하고 있군요! 본인의 수준을 깨닫고, 보다 현실적인 목표를 잡아보세요!" << std::endl;
+		std::cout << _sub.GetSubName() << " 과목에 대해 당신은 당신 스스로를 과대평가하고 있군요! 본인의 수준을 깨닫고, 보다 현실적인 목표를 잡아보세요!" << std::endl;
 	}
 	else if ( score > ExpScore && ( score - ExpScore ) / score > 0.3 ) { //성취도 < 시험 점수 && 오차가 일정 이상일 때 =>본인의 과소평가
-		std::cout << 과목명 << " 과목에 대해 당신은 당신 스스로를 과소평가하고 있군요! 자신감을 갖고, 보다 높은 목표를 잡아보세요!" << std::endl;
+		std::cout << _sub.GetSubName() << " 과목에 대해 당신은 당신 스스로를 과소평가하고 있군요! 자신감을 갖고, 보다 높은 목표를 잡아보세요!" << std::endl;
 	}
 	else //너 스스로를 알라
-		std::cout << 과목명 << " 과목에 대해 성취도에 맞는 점수를 얻으셨군요! 스스로를 아는 것 역시 아주 좋은 자세입니다! 이 자세를 유지하고 열심히 노력하세요!" << std::endl;
+		std::cout << _sub.GetSubName() << " 과목에 대해 성취도에 맞는 점수를 얻으셨군요! 스스로를 아는 것 역시 아주 좋은 자세입니다! 이 자세를 유지하고 열심히 노력하세요!" << std::endl;
 	std::cout << "--------------------------------------------------------" << std::endl;
 }
 
