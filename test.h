f6d251a2925a786d267c3b1545aa168807cf2369
yooshin0303 <73891259+yooshin0303@users.noreply.github.com>
2020-12-02 21:41:26 +0900

    Update test.h

--- test.h
+++ test.h
@@ -1,12 +1,20 @@
 #pragma once
 #include<iostream>
+#include "subject.h"
+#include "person.h"
 
 #ifndef TEST
 #define TEST
 
+class Person;
+
 class Test {
-	void TodayResult();
-	void AfterTest();
+public:
+	Test() {};
+	~Test() {};
+
+	void TodayResult(Subject& _sub, Person& _person);
+	void AfterTest(Subject& _sub, int score);
 };
 
 #endif // !TEST
