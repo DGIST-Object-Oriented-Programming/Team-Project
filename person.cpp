6747b05a7a68b3c0060d50f49a95318c25ed854d
yooshin0303 <73891259+yooshin0303@users.noreply.github.com>
2020-12-20 09:38:27 +0900

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+
 void Person::setdaylist() {
 	std::cout << "How many subjects do u have? : "; 
 	std::cin >> subject_num; std::cout << std::endl;
@@ -15,23 +16,25 @@ void Person::setdaylist() {
 	std::cout << "----------------------------------------------------------------------------------" << std::endl << std::endl;
 	std::cout << "please give me Subject's information" << std::endl<<std::endl;
 	for (int i = 0; i < subject_num; i++) {
-		std::cout << "Enter your Subject name : "; std::cin >> subs_name; std::cout << std::endl;
+		std::cout << "Enter your Subject name : "; std::cin>>subs_name; std::cout << std::endl;
 
-		std::cout << "Enter your Subject's efficient" <<  " (please put 0 if you can't decide) :  ";
-		std::cin >> subs_eff; std::cout << std::endl;
-
-		// eff가 0~1 사이에 없을 경우
-		while (subs_eff > 1 || subs_eff < 0) {
-			std::cout << "Please entet again. (Eff must be in 0~1) : "; std::cin >> subs_eff; std::cout << std::endl;
+		while (true) {
+			try {
+				std::cout << "Enter your Subject's efficient (please put 0 if you can't decide) :  ";
+				std::cin >> subs_eff;
+				if (std::cin.fail() || subs_eff > 1 || subs_eff < 0) throw subs_eff;
+				break;
+			}
+			catch (float f) {
+				std::cout << "(ERROR please entet again. Eff must be in 0~1)" << std::endl << std::endl;
+				std::cin.clear();
+				std::cin.ignore(256, '\n');
+			}
 		}
-
+		std::cout << std::endl;
 		subjects[i].SetSub(subs_name);
-		if (subs_eff == 0) {
-			subjects[i].SetEff(0.25);
-		}
-		else {
-			subjects[i].SetEff(subs_eff);
-		}
+		if (subs_eff == 0) { subjects[i].SetEff(0.25); }
+		else { subjects[i].SetEff(subs_eff); }
 	}
 	std::cout << "----------------------------------------------------------------------------------" << std::endl;
 }
@@ -39,14 +42,15 @@ void Person::setdaylist() {
 
 Person::Person(int a) {
 	while (true) {
+
 		try {
-			std::cout << "Enter your name : "; // 이름
-			std::cin >> name;
+			std::cout << "Enter your name : ";
+			std::cin>> name;
 			if (std::cin.fail()) throw name;
 			break;
 		}
 		catch (std::string str) {
-			std::cout << "ERROR please enter again (It should be string)" << std::endl << std::endl;
+			std::cout << "(ERROR please enter again. It should be string)" << std::endl << std::endl;
 			std::cin.clear();
 			std::cin.ignore(256, '\n');
 		}
@@ -60,7 +64,7 @@ Person::Person(int a) {
 			break;
 		}
 		catch (int i) {
-			std::cout << "ERROR please enter again (It should be number)" << std::endl << std::endl;
+			std::cout << "(ERROR please enter again. It should be number)" << std::endl << std::endl;
 			std::cin.clear();
 			std::cin.ignore(256, '\n');
 		}
@@ -77,15 +81,9 @@ Person& Person::instance(int a) {
 	return *instance;
 }
 
+
 Person::~Person() {
 	delete[] subjects;
-	std::cout << std::endl;
-	std::cout << "▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣" << std::endl;
-	std::cout << "▣                                                                              ▣" << std::endl;
-	std::cout << "▣  Thank you for using our program. Bye                                        ▣" << std::endl;
-	std::cout << "▣  Good Luck!                                                                  ▣" << std::endl;
-	std::cout << "▣                                                                              ▣" << std::endl;
-	std::cout << "▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣" << std::endl << std::endl;;
 }
 
 
